39e01f187edc64f94048485a3d402eb60b9b291b
kshyun <kshyun@taihoinst.com>
2025-04-29 12:49:35 +0900

    ch1, ch2

---
+++ ch2/src/main/java/com/fastcampus/ch2/HelloController.java
@@ -0,0 +1,15 @@
+package com.fastcampus.ch2;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+//1. 원격 프로그램으로 등록
+//2. url과 메서드 연결
+@Controller
+public class HelloController { //원격 프로그램
+    @RequestMapping("/hello")
+    public void main(){
+        System.out.println("hello");
+    }
+}
